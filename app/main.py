b22b9b2e5244558f61b8015457211eaafcf27f05
Alza Dev <emprebranddigital@gmail.com>
2026-08-19 20:38:42 -0500

    Mejoras de profesionalismo: meta tags OG/Twitter, pagina 404 personalizada, robots.txt, logo responsivo, seccion como funciona

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.config import SECRET_KEY
@@ -13,6 +15,8 @@ app = FastAPI(title="Alza")
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, same_site="lax")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+templates = Jinja2Templates(directory="app/templates")
+
 app.include_router(onboarding_routes.router)
 app.include_router(dashboard_routes.router)
 
@@ -20,3 +24,13 @@ app.include_router(dashboard_routes.router)
 @app.on_event("startup")
 def _arrancar_actualizacion_de_mercado():
     iniciar_actualizacion_periodica()
+
+
+@app.exception_handler(404)
+async def pagina_no_encontrada(request: Request, exc):
+    return templates.TemplateResponse("error_404.html", {"request": request}, status_code=404)
+
+
+@app.get("/robots.txt", response_class=PlainTextResponse, include_in_schema=False)
+def robots():
+    return "User-agent: *\nAllow: /\n"
